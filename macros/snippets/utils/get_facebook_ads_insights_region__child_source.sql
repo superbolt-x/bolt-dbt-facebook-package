274b1b9f9dee5caa1e1e9961d6b95f40879adef1
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-25 19:39:53 +0100

    Create get_facebook_ads_insights_region__child_source.sql

---
+++ macros/snippets/utils/get_facebook_ads_insights_region__child_source.sql
@@ -0,0 +1,42 @@
+{%- macro get_facebook_ads_insights_region__child_source(table_name) -%}
+
+
+{%- set action_types = dbt_utils.get_column_values(source('facebook_raw','ads_insights_'~table_name),'action_type') -%}
+{%- set attributions = ['_1_d_view','_7_d_click'] -%}
+
+SELECT 
+    date,
+    ad_id,
+    _fivetran_id,
+    {% for action_type in action_types -%}
+    {%- set alias = conversion_alias_config(action_type~table_name.split('action')[1].split('s')[0]) if 'action' in table_name else conversion_alias_config(action_type~table_name.split('conversion')[1].split('s')[0]) -%}
+    {%- if alias|length %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN value ELSE 0 END), 0) as {{alias}},
+        {%- for attribution in attributions %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN "{{attribution}}" ELSE 0 END), 0) as {{alias}}{{attribution}}
+        {%- if not loop.last %},{% endif %}
+        {%- endfor -%}
+    {%- else -%}
+        {%- if 'action' in table_name %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN value ELSE 0 END), 0) as "{{action_type}}{{table_name.split('action')[1].split('s')[0]}}",
+        {%- for attribution in attributions %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN "{{attribution}}" ELSE 0 END), 0) as "{{action_type}}{{table_name.split('action')[1].split('s')[0]}}{{attribution}}"
+        {%- if not loop.last %},{% endif -%}
+        {%- endfor -%}
+        {%- elif 'conversion' in table_name %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN value ELSE 0 END), 0) as "{{action_type}}{{table_name.split('conversion')[1].split('s')[0]}}",
+        {%- for attribution in attributions %}
+        COALESCE(SUM(CASE WHEN action_type = '{{action_type}}' THEN "{{attribution}}" ELSE 0 END), 0) as "{{action_type}}{{table_name.split('conversion')[1].split('s')[0]}}{{attribution}}"
+        {%- if not loop.last %},{% endif -%}
+        {%- endfor -%}
+        {%- endif -%}
+    {%- endif -%}
+    {%- if not loop.last %},{%- endif %}
+
+{% endfor %}
+
+    FROM {{ source('facebook_raw','ads_insights_'~table_name) }}
+
+    GROUP BY 1,2,3
+
+{%- endmacro %}
