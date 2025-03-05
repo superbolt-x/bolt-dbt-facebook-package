edc2ababa09e337bd7ce3330d47126d269c341b7
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-05 23:58:33 +0100

    Update facebook_performance_by_campaign.sql

--- models/reporting/facebook_performance_by_campaign.sql
+++ models/reporting/facebook_performance_by_campaign.sql
@@ -32,7 +32,7 @@ WITH
     {%- endfor %}
 
     campaigns AS 
-    (SELECT account_id, campaign_id, campaign_name, campaign_effective_status
+    (SELECT account_id, campaign_id::varchar as campaign, campaign_name, campaign_effective_status
     FROM {{ ref('facebook_campaigns') }}
     )
 
