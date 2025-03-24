1ac87504728ebcf0f46fa5dd5c7fca8f60992d1d
Vincent Gefflaut <vincent.gefflaut@superbolt.agency>
2025-03-24 11:57:10 -0400

    consolidated bis -> consolidated

--- models/reporting/facebook_performance_by_ad_consolidated.sql
+++ models/reporting/facebook_performance_by_ad_consolidated.sql
@@ -1,7 +1,6 @@
 {{ config (
-    unique_key = 'unique_id',
-    on_schema_change = 'append_new_columns',
     alias = target.database + '_facebook_performance_by_ad_consolidated'
+
 )}}
 
 {%- set currency_fields = [
@@ -45,8 +44,8 @@
                     |reject("in",exclude_fields)
                     -%}  
 
-WITH  
-    {%- if var('currency') != 'USD' -%}
+WITH 
+    {% if var('currency') != 'USD' -%}
     currency AS
     (SELECT DISTINCT date, "{{ var('currency') }}" as raw_rate, 
         LAG(raw_rate) ignore nulls over (order by date) as exchange_rate
@@ -55,10 +54,10 @@ WITH
     WHERE date <= current_date),
     {%- endif -%}
 
-    {%- set exchange_rate = 1 if var('currency') == 'USD' else 'exchange_rate' -%}
+    {%- set exchange_rate = 1 if var('currency') == 'USD' else 'exchange_rate' %}
     
-    insights AS (
-    SELECT 
+    insights AS 
+    (SELECT 
         {%- for field in stg_fields if (("_1_d_view" not in field and "_7_d_click" not in field) or ("purchases" in field or "revenue" in field)) -%}
         {%- if field in currency_fields or '_value' in field %}
         "{{ field }}"::float/{{ exchange_rate }} as "{{ field }}"
@@ -73,15 +72,12 @@ WITH
     {%- endif %}
     ),
 
-    facebook_ads_insights AS (
-    SELECT
-        *,
-        {{ get_date_parts('date') }}
-    FROM
-        insights
-    ),
+    insights_stg AS 
+    (SELECT *,
+    {{ get_date_parts('date') }}
+    FROM insights),
 
--- facebook_ads
+
 {%- set selected_fields = [
     "id",
     "name",
@@ -90,27 +86,18 @@ WITH
     "updated_time"
 ] -%}
 {%- set schema_name, table_name = 'facebook_raw', 'ads' -%}
-    ads_staging AS (
-    SELECT
+
+    ads_staging AS 
+    (SELECT
+    
         {% for field in selected_fields -%}
         {{ get_facebook_clean_field(table_name, field) }},
         {% endfor -%}
         MAX(updated_time) OVER (PARTITION BY id) as last_updated_time
+
     FROM {{ source(schema_name, table_name) }}
-    -- Dimensions are generally not incrementally filtered
     ),
 
-    facebook_ads AS (
-    SELECT
-        *,
-        ad_id AS unique_key
-    FROM
-        ads_staging
-    WHERE
-        updated_time = last_updated_time
-    ),
-
--- facebook_adsets
 {%- set selected_fields = [
     "id",
     "name",
@@ -124,24 +111,17 @@ WITH
 ] -%}
 {%- set schema_name, table_name = 'facebook_raw', 'adsets' -%}
 
-adsets_staging AS (
-    SELECT
+    adsets_staging AS 
+    (SELECT
+    
         {% for field in selected_fields -%}
         {{ get_facebook_clean_field(table_name, field) }},
         {% endfor -%}
         MAX(updated_time) OVER (PARTITION BY id) as last_updated_time
+
     FROM {{ source(schema_name, table_name) }}
-),
+    ),
 
-facebook_adsets AS (
-    SELECT
-        *,
-        adset_id as unique_key
-    FROM adsets_staging 
-    WHERE updated_time = last_updated_time
-),
-
--- facebook_campaigns
 {%- set selected_fields = [
     "id",
     "name",
@@ -152,37 +132,31 @@ facebook_adsets AS (
 ] -%}
 {%- set schema_name, table_name = 'facebook_raw', 'campaigns' -%}
 
-campaigns_staging AS (
-    SELECT
+    campaigns_staging AS 
+    (SELECT
+    
         {% for field in selected_fields -%}
         {{ get_facebook_clean_field(table_name, field) }},
         {% endfor -%}
         MAX(updated_time) OVER (PARTITION BY id) as last_updated_time
+
     FROM {{ source(schema_name, table_name) }}
-),
+    ),
 
-facebook_campaigns AS (
-    SELECT
-        *,
-        campaign_id as unique_key
-    FROM campaigns_staging
-    WHERE updated_time = last_updated_time
-),
-
--- Performances by granularity
-{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
 {%- set date_granularity_list = ['day','week','month','quarter','year'] -%}
+{%- set exclude_fields = ['date','day','week','month','quarter','year','last_updated','unique_key'] -%}
 {%- set dimensions = ['account_id','campaign_id','adset_id','ad_id','attribution_setting'] -%}
 {%- set measures = adapter.get_columns_in_relation(ref('facebook_ads_insights'))
                     |map(attribute="name")
                     |reject("in",exclude_fields)
                     |reject("in",dimensions)
                     |list
-                    -%}
+                    -%}  
+ 
+    {%- for date_granularity in date_granularity_list %}
 
-{%- for date_granularity in date_granularity_list %}
-performance_{{date_granularity}} AS (
-    SELECT 
+    performance_{{date_granularity}} AS 
+    (SELECT 
         '{{date_granularity}}' as date_granularity,
         {{date_granularity}} as date,
         {%- for dimension in dimensions %}
@@ -192,34 +166,37 @@ performance_{{date_granularity}} AS (
         COALESCE(SUM("{{ measure }}"),0) as "{{ measure }}"
         {%- if not loop.last %},{%- endif %}
         {% endfor %}
-    FROM facebook_ads_insights
-    GROUP BY {{ range(1, dimensions|length +2 +1)|list|join(',') }}
-){%- if not loop.last %},{% endif %}
-{% endfor %},
+    FROM insights_stg
+    GROUP BY {{ range(1, dimensions|length +2 +1)|list|join(',') }}),
+    {%- endfor %}
 
--- Combines all the performances
-combined_performance AS (
-    {% for date_granularity in date_granularity_list -%}
+    ads AS
+    (SELECT account_id, ad_id::varchar as ad_id, ad_name, ad_effective_status
+    FROM ads_staging 
+    WHERE updated_time = last_updated_time),
+    
+    adsets AS
+    (SELECT account_id, adset_id, adset_name, adset_effective_status
+    FROM adsets_staging 
+    WHERE updated_time = last_updated_time),
+    
+    campaigns AS
+    (SELECT account_id, campaign_id, campaign_name, campaign_effective_status
+    FROM campaigns_staging 
+    WHERE updated_time = last_updated_time)
+
+SELECT *,
+    {{ get_facebook_default_campaign_types('campaign_name')}},
+    {{ get_facebook_scoring_objects() }}
+FROM 
+    ({% for date_granularity in date_granularity_list -%}
     SELECT *
     FROM performance_{{date_granularity}}
-    {% if not loop.last %}UNION ALL{% endif %}
-    {%- endfor %}
-)
+    {% if not loop.last %}UNION ALL
+    {% endif %}
 
--- Final request to combine all the data
-SELECT 
-    cp.*,
-    a.ad_name,
-    a.ad_effective_status,
-    ads.adset_name,
-    ads.adset_effective_status,
-    c.campaign_name,
-    c.campaign_effective_status,
-    {{ get_facebook_default_campaign_types('c.campaign_name')}},
-    {{ get_facebook_scoring_objects() }},
-    -- Unique key to identify the row
-    COALESCE(date_granularity, '') || '_' || COALESCE(date::varchar, '') || '_' || COALESCE(ad_id, '') as unique_id
-FROM combined_performance cp
-LEFT JOIN facebook_ads a ON cp.ad_id = a.ad_id AND cp.account_id = a.account_id
-LEFT JOIN facebook_adsets ads ON cp.adset_id = ads.adset_id AND cp.account_id = ads.account_id
-LEFT JOIN facebook_campaigns c ON cp.campaign_id = c.campaign_id AND cp.account_id = c.account_id
+    {%- endfor %}
+    )
+LEFT JOIN ads USING(account_id,ad_id)
+LEFT JOIN adsets USING(account_id,adset_id)
+LEFT JOIN campaigns USING(account_id,campaign_id)
